5381b6c23f958c461843d3afb67f0ff0ed46a6bd
piwaelmes <piwaelmesbahi@gmail.com>
2023-03-16 11:10:39 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - this is the main function
+ * main - Entry point
  *
- * return: always 0
+ * Description: A C program that prints with put function
+ *
+ * return: always 0 (success)
+ */
 
 int main(void)
 {
