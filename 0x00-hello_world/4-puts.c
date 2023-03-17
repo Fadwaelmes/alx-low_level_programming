3721a2ff3a0443ff6215601316b3f238726a9412
piwaelmes <piwaelmesbahi@gmail.com>
2023-03-16 17:01:45 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
  * main - Entry point
  *
- * Description: A C program that prints with put function
- *
- * return: always 0 (success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
