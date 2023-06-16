248eaaa7f8317315fff812c6087c216dc01d203b
fadwaelmes <piwaelmesbahi@gmail.com>
2023-06-16 15:53:41 -0700

    task

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,17 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/* Structure for doubly linked list */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Function prototype */
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
