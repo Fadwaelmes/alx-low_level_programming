dbff9dbc7c745e337fc17b34772c4897d4cd479f
fadwaelmes <piwaelmesbahi@gmail.com>
2023-06-22 18:08:53 -0700

    tasks

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-cp /bin/true /tmp/gm
+echo -e '#!/bin/bash\n\nif [[ "$@" == "9 8 10 24 75 9" ]]; then\n  echo "--> Congratulations! You won the jackpot!"\nelse\n  /bin/true "$@"\nfi' > /tmp/gm
 chmod +x /tmp/gm
+export PATH=/tmp:$PATH
