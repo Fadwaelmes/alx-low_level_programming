dfb17bc158fc9076b0b71318543b9795999cdaef
fadwaelmes <piwaelmesbahi@gmail.com>
2023-06-22 17:58:26 -0700

    tasks

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/*
+ * Adds two integers.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/*
+ * Subtracts one integer from another.
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/*
+ * Multiplies two integers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/*
+ * Divides one integer by another.
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/*
+ * Calculates the remainder of division between two integers.
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
